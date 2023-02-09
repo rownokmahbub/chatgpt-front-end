af20ddad3fb200ee1f6306f63cc8751e9aed99ae
Rownok <rownok2001@gmail.com>
2023-02-09 16:37:46 +0600

    upload

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const fetchResponse =  async(chat) => {
     try {
         // after depoloyment you should change the fetch URL below
-        const response = await fetch('https://chatgpt-backend-eight.vercel.app', { 
+        const response = await fetch('https://chatgpt-backend-delta.vercel.app/', { 
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
